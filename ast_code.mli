0395cd83e99003af089a2c3d13936b4860346f49
sieunhanbom04 <31831447+sieunhanbom04@users.noreply.github.com>
2018-01-01 23:56:42 +0100

    Add files via upload

--- ast_code.mli
+++ ast_code.mli
@@ -19,24 +19,29 @@ and pexpr =
   | PEbool of int
   | PEvar of int * int
   | PEbinop of binop * pexpr * pexpr
+  | PEassign of pexpr * pexpr * int
+              (*right and left expression with the size of expression*)
   | PEunop of unop * pexpr
   | PEdereference of pexpr * int
               (*expression, the true size of the variable*)
-  | PEreference of int
-              (*the relative position with rbp*)
+  | PEreference of int * pexpr
+              (*size of expression and expression*)
   | PEstruct of pexpr * int * int * int * bool
               (*expression, size of struct, location of field, size of field, auto_dereference or not*)
-  | PElength of pexpr
-  | PEindex of pexpr * pexpr 
+  | PElength of pexpr * bool
+              (*vector, auto_dereference or not*)
+  | PEindex of pexpr * pexpr * int * bool
+              (*vector, index, auto_dereference or not*)
   | PEprint of string
   | PEcall of ident * pexpr list * int
-  | PEvector of int * pexpr list
+  | PEvector of int * int * pexpr list
+              (*number of elements, size of index, list of expression*)
   | PEblock of pblock
 and pblock =
   | PCFullBlock of pinstruction list * pexpr
   | PCBlock of pinstruction list
 and pcondition =
-  | PCif of pexpr * pblock * pblock
+  | PCif of pexpr * pblock * (pblock option)
   | PCnestedIf of pexpr * pblock * pcondition
 
 type decl_pfun = {
